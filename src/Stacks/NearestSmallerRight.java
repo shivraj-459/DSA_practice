423cf44523816a29601982b36c594fc9f7658016
shivraj-459 <shivrajpunjare459@gmail.com>
2022-10-06 16:49:37 +0530

    added

---
+++ src/Stacks/NearestSmallerRight.java
@@ -0,0 +1,52 @@
+package Stacks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class NearestSmallerRight {
+
+    public static List<Integer> NearestSmaller(int[] arr, int size){
+
+        Stack<Integer> stack=new Stack<>();
+
+        List<Integer> list=new ArrayList<>();
+
+        for(int i=size-1;i>=0;i--){
+
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
+
+                stack.pop();
+            }
+
+            if(stack.isEmpty()){
+
+                list.add(-1);
+            }
+
+            if(!stack.isEmpty() && stack.peek()<arr[i]){
+
+                list.add(stack.peek());
+            }
+
+            stack.push(arr[i]);
+        }
+
+        Collections.reverse(list);
+
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr={4,5,2,10,8};
+
+        int size=arr.length;
+
+        List<Integer> output=NearestSmaller(arr,size);
+        System.out.println(output);
+
+    }
+}
